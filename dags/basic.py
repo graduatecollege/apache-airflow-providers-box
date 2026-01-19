c25832497f6e4d25d1cba2a5dcc1b62922e21621
Mikael Arvola <marvel@illinois.edu>
2026-01-19 10:34:52 -0600

    Add comments to basic.py examples

--- dags/basic.py
+++ dags/basic.py
@@ -20,28 +20,44 @@ def basic():
     A basic DAG to demonstrate Box operators and sensors.
     """
 
+    # Sensor to wait for a file to be updated
     wait_for_update = BoxSensor(
         task_id="wait_for_update",
         box_conn_id="box",
+
+        # Path to the folder or file to wait for
         path="/Grad Automation/GradCollegeTicketStatusReport.xlsx",
-        newer_than='{{ dag_run.logical_date + macros.timedelta(hours=-6) }}',
+        # If providing a folder in `path`, use file_pattern to match files
+        #file_pattern="*.xlsx",
+
+        # Datetime to compare against the file's last modified time. This can
+        # be a string that pendulum can parse, or a datetime object.
+        newer_than='{{ dag_run.logical_date + macros.timedelta(hours=-1) }}',
+
         deferrable=True,
         timeout=timedelta(hours=1),
     )
 
     @task()
     def extract():
+        """
+        BoxHook can also be used directly to extract data from Box.
+        """
         box_hook = BoxHook(box_conn_id="box")
         response = box_hook.get_file_id("/Grad Automation/GradCollegeTicketStatusReport.xlsx")
         return response
 
     @task()
     def last_modified():
+        """
+        Here we use BoxHook to get the last modified time of a file.
+        """
         box_hook = BoxHook(box_conn_id="box")
         response = box_hook.get_file_modified_time("/Grad Automation/GradCollegeTicketStatusReport.xlsx")
 
         return response[1]
 
+    # Download a file from Box
     down = BoxDownloadOperator(
         task_id="download",
         box_conn_id="box",
@@ -49,6 +65,7 @@ def basic():
         box_path="/Grad Automation/GradCollegeTicketStatusReport.xlsx",
     )
 
+    # Upload a file to Box. This will update the file if it already exists.
     upload_to_box = BoxUploadOperator(
         task_id="upload_to_box",
         local_path="./GradCollegeTicketStatusReport.xlsx",
